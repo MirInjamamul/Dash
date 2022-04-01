578b7197dceac48742a7feecbb15a88d025b583d
MirInjamamul <injamul120221@gmail.com>
2022-04-01 23:29:48 +0600

    customized css added with dash application

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import plotly.express as px
 
 app = Dash(__name__)
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "JackFruit", "Oranges"],
     "Amount": [4, 1, 2, 2, 4, 2],
@@ -12,13 +17,27 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+fig.update_layout(
+    plot_bgcolor= colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(children='Hello Dash',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+    ),
 
     html.Div(
-        children='''
-            Dash: A web application framework for your data.
-        '''),
+        children='Dash: A web application framework for your data.',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
 
     dcc.Graph(
         id='example graph',
